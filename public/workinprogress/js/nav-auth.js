ce34505d6a7f76394cd822803d78541a795619d5
Andrew Bubis <andrewbubis@gmail.com>
2026-08-21 13:59:13 -0500

    Fix grants dropdown link to go directly to grant tracker

--- public/workinprogress/js/nav-auth.js
+++ public/workinprogress/js/nav-auth.js
@@ -60,7 +60,7 @@
           (me.isAdmin ? '<li><a href="invites.html">send link</a></li>' : '') +
           '<li><a href="sales.html">sales</a></li>' +
           '<li><a href="inquiries.html">inquiries</a></li>' +
-          '<li><a href="profile.html?tab=grants">grants</a></li>' +
+          '<li><a href="grants.html">grants</a></li>' +
           '<li><a href="documents.html">documents</a></li>' +
           '<li><a href="settings.html">settings</a></li>' +
           '<li class="sep"><a href="#" data-logout>log out</a></li>' +
